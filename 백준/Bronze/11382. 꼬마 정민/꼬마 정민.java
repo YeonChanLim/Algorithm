7db14ea775c5429943f86ff1b0c6d084bf9cfb19
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-03-28 17:13:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static long a,b,c;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextLong();
+		b = sc.nextLong();
+		c = sc.nextLong();
+		
+		System.out.println(a+b+c);
+	}
+
+}
