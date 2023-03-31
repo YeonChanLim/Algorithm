e087caa4c772e88871441ce6043169766830316c
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-03-31 12:56:24 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int T, A, B;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		T = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < T; i++) {
+
+			st = new StringTokenizer(br.readLine());
+
+			 A = Integer.parseInt(st.nextToken());
+			 B = Integer.parseInt(st.nextToken());
+
+			System.out.println(A + B); 
+//			System.out.println("\n");
+		}
+	}
+
+}
