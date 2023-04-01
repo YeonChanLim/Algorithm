926c51859e67eb592abcf6d909824fe4fbfbabb8
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-04-01 23:27:15 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int A, B;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		while (true) {
+			// st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+
+			if (A + B == 0) {
+				break;
+			}
+			bw.write(Integer.toString(A + B));
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
