5e0faa5480a8029ab7cd4edde2a7c7f562a42d83
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-04-12 18:17:09 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		int sum = 0, max = 0;
+
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+
+		}
+
+		System.out.println((double)sum*100/max/N);
+	}
+
+}
+
