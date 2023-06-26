79cd40163c02607535b3e400ef7b57082ccf62a6
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-06-26 14:26:09 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -3,18 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	static int a, b;
-	
-	public static void main(String args[]) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main{
+
+    static int a, b;
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
-        
+
         System.out.println(a+b);
-	}
+    }
 
 }
