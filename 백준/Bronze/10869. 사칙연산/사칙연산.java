c6616ecb10f4d3ba4a96ca42be24c050eed2fd7e
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-06-27 23:31:54 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -4,21 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int a, b;
-	
-	public static void main(String args[]) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int a,b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
-        
+
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
         System.out.println(a%b);
-	}
-
+    }
 }
