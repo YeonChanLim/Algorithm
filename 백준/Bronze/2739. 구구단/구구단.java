f7775ac0539f2dc50df5178ca69d9d67f67e4cc3
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-06-27 11:56:30 +0900

    [Bronze V] Title: 구구단, Time: 160 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
 
-		sc.close();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N*i));
 
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(N + " * " + i + " = " + (N * i));
-
-		}
-
-	}
-
+        }
+    }
 }
