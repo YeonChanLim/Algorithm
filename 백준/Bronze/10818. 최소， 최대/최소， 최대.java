9645c78a193e75e85a731e868c33f2b56090df3f
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-07-02 21:50:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 1320 ms, Memory: 104848 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
+    static int N;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
 
-		}
-		Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
 
-		System.out.println((arr[0] + " " + arr[N - 1]));
-	}
+        }
+        Arrays.sort(arr);
+
+        System.out.println((arr[0] + " " + arr[N - 1]));
+    }
 
 }
