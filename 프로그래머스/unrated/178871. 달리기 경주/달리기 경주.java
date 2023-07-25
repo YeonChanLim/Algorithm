c74466316300e45a426aba6845f3709a25479bf8
YeonChanLim <93699241+YeonChanLim@users.noreply.github.com>
2023-07-25 23:56:41 +0900

    [unrated] Title: 달리기 경주, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+        for (String s : callings) {
+            int rank = map.get(s);
+            map.put(s, rank - 1);
+            map.put(players[rank - 1], rank);
+            players[rank] = players[rank -1];
+            players[rank -1] = s;
+        }
+        return players;
+    }
+}
